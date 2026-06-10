742ffbcba4c0eb728f877596f3a9bbbbc521e977
Matteo Collina <matteo.collina@dma.it>
2026-06-10 14:26:01 +0200

    fix

--- apps/storefront/src/pages/OrderDetail/components/OrderHistory.tsx
+++ apps/storefront/src/pages/OrderDetail/components/OrderHistory.tsx
@@ -16,14 +16,6 @@ const HistoryListContainer = styled('div')(() => ({
   '& > .MuiPaper-root': {
     boxShadow: 'none',
   },
-
-  '& table': {
-    '& td, & th': {
-      '&:first-of-type': {
-        paddingLeft: 0,
-      },
-    },
-  },
 }));
 
 export function OrderHistory() {
